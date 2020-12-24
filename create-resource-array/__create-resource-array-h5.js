c0c3a9ad0096667a14b1e46aba2c3fa90b0cedee
quantum rose <cui-haojie@outlook.com>
2020-12-24 16:00:11 +0800

    现在支持递归遍历目标文件夹下的所有后代文件夹，支持忽略不想被导出的文件夹和文件

--- create-resource-array/__create-resource-array-h5.js
+++ create-resource-array/__create-resource-array-h5.js
@@ -1,8 +1,8 @@
 /**
- * 创建项目所需的图片预加载列表，适用于h5，在 webpack 环境中利用 process.env.BASE_URL 以在不同运行环境中使用不同的资源地址
+ * 创建项目所需的图片预加载列表，适用于h5，利用 process.env.BASE_URL 以在不同运行环境中使用不同的资源地址
  *
  * 需在 node 环境中运行！
- * 本工具会读取当前目录下所有图片类文件的名称，并输出到目标文件中
+ * 本工具会深度遍历当前目录下所有图片类文件的名称，并输出到目标文件中
  * 输出文件的内容为 js 代码
  * 示例：“export default [process.env.BASE_URL + 'imgs/loading.png']”
  * 因此可供其他 js 文件以符合 ES6 模块化规范的方式引入
@@ -11,25 +11,30 @@
 const fs = require('fs');
 const path = require('path');
 
+// 要排除的文件或文件夹
+const EXCLUDE = /^dir1|dir2|exclude\.png$/;
 // 输出文件的路径
-const outputFile = path.join(
-  __dirname,
-  '../../src/util/resource-array-loading.js'
-);
-const dirname = __dirname.split('\\').pop();
+const OUTPUT_FILE = path.join(__dirname, '../src/utils/resource-array-loading.js');
 
-fs.readdir(__dirname, (err, files) => {
-  if (err) throw err;
+function deepReaddir(dirPath) {
+    const imgs = [];
+    fs.readdirSync(dirPath).forEach(item => {
+        if (!EXCLUDE.test(item)) {
+            if (fs.lstatSync(path.join(dirPath, item)).isDirectory()) {
+                imgs.push(...deepReaddir(path.join(dirPath, item)));
+            } else {
+                if (/\.jpg|png|gif|bmp$/.test(item)) {
+                    imgs.push(`process.env.BASE_URL + '${dirPath.replace(__dirname, '').replace(/\\/g, '/')}/${item}'`);
+                }
+            }
+        }
+    });
+    return imgs;
+}
 
-  let imgs = files
-    .filter(item => /\.jpg|png|gif|bmp$/.test(item))
-    .map(item => `process.env.BASE_URL + '${dirname}/${item}'`)
-    .join(',\r\n  ');
+const imgs = deepReaddir(__dirname);
 
-  imgs = `export default [\r\n  ${imgs}\r\n]`;
-
-  fs.writeFile(outputFile, imgs, err => {
+fs.writeFile(OUTPUT_FILE, `export default [\r\n    ${imgs.join(',\r\n    ')}\r\n];`, err => {
     if (err) throw err;
     console.log('文件已被保存');
-  });
 });
--- create-resource-array/__create-resource-array-mp.js
+++ create-resource-array/__create-resource-array-mp.js
@@ -2,33 +2,41 @@
  * 创建项目所需的图片预加载列表，适用于微信小程序
  *
  * 需在 node 环境中运行！
- * 本工具会读取当前目录下所有图片类文件的名称，并输出到目标文件中
+ * 本工具会深度遍历当前目录下所有图片类文件的名称，并输出到目标文件中
  * 输出文件的内容为 js 代码
- * 示例：“module.exports = [https://cdn.com/ + 'imgs/loading.png']”
- * 因此可供其他 js 文件以符合 commonjs 模块化规范的方式引入
+ * 示例：“export default ['https://cdn.com/imgs/loading.png']”
+ * 因此可供其他 js 文件以符合 ES6 模块化规范的方式引入
  */
 
 const fs = require('fs');
 const path = require('path');
 
-// cdn 地址
-const cdnUrl = 'https://cdn.com';
+// 要排除的文件或文件夹
+const EXCLUDE = /^dir1|dir2|exclude\.png$/;
+// CDN 地址
+const CDN_URL = 'https://cdn.com';
 // 输出文件的路径
-const outputFile = path.join('D:/wxminiprogram/utils/resource-array-loading.js');
-const dirname = __dirname.split('\\').pop();
+const OUTPUT_FILE = path.join(__dirname, 'resource-array-loading.js');
 
-fs.readdir(__dirname, (err, files) => {
-    if (err) throw err;
-
-    let imgs = files
-        .filter(item => /\.jpg|png|gif|bmp$/.test(item))
-        .map(item => `'${cdnUrl + dirname}/${item}'`)
-        .join(',\r\n  ');
-
-    imgs = `module.exports = [\r\n  ${imgs}\r\n]`;
-
-    fs.writeFile(outputFile, imgs, err => {
-        if (err) throw err;
-        console.log('文件已被保存');
+function deepReaddir(dirPath) {
+    const imgs = [];
+    fs.readdirSync(dirPath).forEach(item => {
+        if (!EXCLUDE.test(item)) {
+            if (fs.lstatSync(path.join(dirPath, item)).isDirectory()) {
+                imgs.push(...deepReaddir(path.join(dirPath, item)));
+            } else {
+                if (/\.jpg|png|gif|bmp$/.test(item)) {
+                    imgs.push(`'${CDN_URL + dirPath.replace(__dirname, '').replace(/\\/g, '/')}/${item}'`);
+                }
+            }
+        }
     });
+    return imgs;
+}
+
+const imgs = deepReaddir(__dirname);
+
+fs.writeFile(OUTPUT_FILE, `export default [\r\n    ${imgs.join(',\r\n    ')}\r\n];`, err => {
+    if (err) throw err;
+    console.log('文件已被保存');
 });
